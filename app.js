b4ebfa8a4eb129b1597b0668423d9cf54a64a92e
bernardoscrespo <162755348+bernardoscrespo@users.noreply.github.com>
2024-03-09 14:32:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function exibirMensagemInicial() {
 exibirMensagemInicial();
 
 function regras() {
-    exibirTextoNaTela('regras', 'As regras do jogo são: 1- Tente adivinhar o número que o computador gerou;');
+    exibirTextoNaTela('headRegras', 'Regras do\njogo:');
 }
 regras();
 
